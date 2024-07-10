51af2057d86043886ceab4fb97c5aae3e1969ec7
Marcos Faneli <marcosfaneli@gmail.com>
2024-07-09 21:22:14 -0300

    feat: Refactoring create order test

--- src/test/java/com/seuprojeto/integrationtest/integration/OrderControllerIntegrationTest.java
+++ src/test/java/com/seuprojeto/integrationtest/integration/OrderControllerIntegrationTest.java
@@ -1,5 +1,8 @@
 package com.seuprojeto.integrationtest.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.seuprojeto.integrationtest.app.controller.dto.CreateOrderDto;
+import com.seuprojeto.integrationtest.app.controller.dto.OrderCreatedDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -10,7 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.regex.Pattern;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -20,6 +26,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderControllerIntegrationTest {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     private static final String ORDER_URL = "/orders";
 
@@ -35,18 +43,24 @@ class OrderControllerIntegrationTest {
 
     @Test
     void shouldReturn201WhenCreateOrder() throws Exception {
-        var response = mockMvc.perform(MockMvcRequestBuilders.post(ORDER_URL).content("{\"description\": \"some description\"}").contentType(MediaType.APPLICATION_JSON))
+        final String description = "some description";
+        final CreateOrderDto createOrderDto = new CreateOrderDto(description);
+        final String payload = objectMapper.writeValueAsString(createOrderDto);
+
+        final MvcResult response = this.mockMvc.perform(MockMvcRequestBuilders.post(ORDER_URL).content(payload).contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andReturn();
 
-        System.out.println(response.getResponse().getContentAsString());
+        OrderCreatedDto orderCreatedDto = objectMapper.readValue(response.getResponse().getContentAsString(), OrderCreatedDto.class);
 
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("description"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("some description"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("id"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("createdAt"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("updatedAt"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("status"));
-        Assertions.assertTrue(response.getResponse().getContentAsString().contains("OPENED"));
+        Pattern pattern = Pattern.compile("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$");
+
+        Assertions.assertNotNull(orderCreatedDto.id());
+        Assertions.assertTrue(pattern.matcher(orderCreatedDto.id()).matches());
+
+        Assertions.assertEquals(description, orderCreatedDto.description());
+        Assertions.assertNotNull(orderCreatedDto.createdAt());
+        Assertions.assertNotNull(orderCreatedDto.updatedAt());
+        Assertions.assertEquals("OPENED", orderCreatedDto.status());
     }
 }
