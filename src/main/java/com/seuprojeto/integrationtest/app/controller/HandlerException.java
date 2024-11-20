850b4040eb08507d76a6d42fe851d4c6d0244bcd
Marcos Faneli <marcosfaneli@gmail.com>
2024-11-20 16:50:54 -0300

    feat: Added swagger config

--- src/main/java/com/seuprojeto/integrationtest/app/controller/HandlerException.java
+++ src/main/java/com/seuprojeto/integrationtest/app/controller/HandlerException.java
@@ -1,6 +1,7 @@
 package com.seuprojeto.integrationtest.app.controller;
 
 import com.seuprojeto.integrationtest.domain.CustomerNotFoundException;
+import com.seuprojeto.integrationtest.domain.ErrorMessage;
 import com.seuprojeto.integrationtest.domain.OrderNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,35 +9,29 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class HandlerException {
 
-    private Map<String, String> createMap(String message) {
-        final Map<String, String> map = new HashMap<>();
-        map.put("message", message);
-        map.put("timestamp", Instant.now().toString());
-
-        return map;
+    private ErrorMessage createMap(String message) {
+        return new ErrorMessage(message, Instant.now());
     }
 
     @ExceptionHandler(OrderNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public Map<String, String> handleOrderNotFoundException(OrderNotFoundException e) {
+    public ErrorMessage handleOrderNotFoundException(OrderNotFoundException e) {
         return this.createMap(e.getMessage());
     }
 
     @ExceptionHandler(CustomerNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
-    public Map<String, String> handleCustomerNotFoundException(CustomerNotFoundException e) {
+    public ErrorMessage handleCustomerNotFoundException(CustomerNotFoundException e) {
         return this.createMap(e.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
-    public Map<String, String> handleException(Exception e) {
+    public ErrorMessage handleException(Exception e) {
         System.out.println(e.getClass());
         System.out.println(e.getMessage());
         return this.createMap("Internal server error");
