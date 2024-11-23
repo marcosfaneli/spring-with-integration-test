3388952cb10869e68d33b63aa7902344e67f205a
Marcos Faneli <marcosfaneli@gmail.com>
2024-11-23 17:40:32 -0300

    feat: config environment

--- src/main/java/com/seuprojeto/integrationtest/shared/CorsConfig.java
+++ src/main/java/com/seuprojeto/integrationtest/shared/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.seuprojeto.integrationtest.shared;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,8 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${web.cors.allowed-origins}")
+    private String corsFilter;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping(this.corsFilter);
     }
 }
